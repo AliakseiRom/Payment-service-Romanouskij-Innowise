c67f93a3c1e6905f3a6eeb75ea008da1c2b6f5ba
Aliaksei <aliaksei@gmail.com>
2026-06-22 23:35:33 +0300

    Add PaymentRepository

---
+++ src/main/java/com/innowise/paymentservice/repository/PaymentRepository.java
@@ -0,0 +1,9 @@
+package com.innowise.paymentservice.repository;
+
+import com.innowise.paymentservice.model.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+}
