e641b00c63193a8ea7bdd46c0dee9241eb4f049b
Aliaksei <aliaksei@gmail.com>
2026-06-22 23:01:11 +0300

    Add Payment entity with enum class Status

---
+++ src/main/java/com/innowise/paymentservice/model/Payment.java
@@ -0,0 +1,31 @@
+package com.innowise.paymentservice.model;
+
+import com.innowise.paymentservice.util.Status;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "order_id")
+    private Long orderId;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @Column(name = "payment_timestamp")
+    private LocalDateTime timestamp;
+
+    @Column(name = "payment_amount")
+    private Long paymentAmount;
+}
---
+++ src/main/java/com/innowise/paymentservice/util/Status.java
@@ -0,0 +1,11 @@
+package com.innowise.paymentservice.util;
+
+public enum Status {
+
+    PENDING,
+    PROCESSING,
+    PAID,
+    FAILED,
+    CANCELLED,
+    REFUNDED,
+}
